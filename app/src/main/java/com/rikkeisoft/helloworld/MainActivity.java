d9adaecdef6f6faaa251bcc7c3b6bafb63ea15b3
Kiên Bùi <khomtrexanh@gmail.com>
2017-10-24 18:18:09 +0700

    main acti

--- app/src/main/java/com/rikkeisoft/helloworld/MainActivity.java
+++ app/src/main/java/com/rikkeisoft/helloworld/MainActivity.java
@@ -10,9 +10,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 		
-		/* int i = 0;
-		if (1+1 == 2)
-			i = 10; */
-		
+		int i = 0;
     }
 }
